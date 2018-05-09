22ab7c5521751cd43f6ad6d640403dfe5c689066
David Hoekman <sqrtneginf@gmail.com>
2018-05-09 16:54:25 -0700

    Java OK, no bug

--- SquaredvsMultiplied.java
+++ SquaredvsMultiplied.java
@@ -1,15 +1,15 @@
 public class SquaredvsMultiplied
 {
- public static void main(String[] args)
- {
+  public static void main(String[] args)
+  {
 
-double x = .05;
-double y = 1.4;
+    double x = .05;
+    double y = 1.4;
 
-for (int i = 0; i < 100000; i++) {
-    x = y - x * x;
-    if ((x*x) != Math.pow(x,2)) 
-         System.out.println("i = " + i + " x = " + x);
-}
-}
+    for (int i = 0; i < 100000; i++) {
+        x = y - x * x;
+        if ((x*x) != Math.pow(x,2)) 
+            System.out.println("i = " + i + " x = " + x);
+    }
+  }
 }
